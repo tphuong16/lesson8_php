7cab538d0596385edd3823f0e8adadfbfb0decb8
tphuong16 <132880191+tphuong16@users.noreply.github.com>
2023-07-03 22:18:35 +0700

    Create pdo.php

---
+++ product/pdo.php
@@ -0,0 +1,35 @@
+<?php
+    require_once "../category/pdo.php";
+
+    // Lấy dữ liệu 
+    function getProdData(){
+        $sql = "SELECT * FROM product INNER JOIN categories ON product.cateId = categories.id";
+        $select = prepareSQL($sql);
+        $select->execute();
+        return $select->fetchAll();
+    }
+
+    function getOneProdData($data){
+        $sql = "SELECT * FROM product WHERE prodId = :id";
+        $select = prepareSQL($sql);
+        $select->execute($data);
+        return $select->fetchAll();
+    }
+
+    function createNewProdData($data){
+        $sql = "INSERT INTO product (prodId,prodName,prodPrice,cateId)VALUES (:prodId, :prodName, :prodPrice, :cateId)";
+        $create = prepareSQL($sql);
+        $create->execute($data);
+    }
+
+    function updateProdData($data){
+        $sql = "UPDATE product SET  prodName = :prodName, prodPrice = :prodPrice, cateId = :cateId  WHERE prodId = :id";
+        $update = prepareSQL($sql);
+        $update->execute($data);
+    }
+    function deleteProdData($data){
+        $sql = "DELETE FROM product WHERE prodId = :id";
+        $update = prepareSQL($sql);
+        $update->execute($data);
+    }
+?>
